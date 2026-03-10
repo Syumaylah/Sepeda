4837c133c9731ce1d91076aa04919cdc9bbe905f
Aurelya16 <aurelyanasywa16@gmail.com>
2026-03-10 15:56:27 +0700

    upload API

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,5 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}',  [ProductController::class, 'update']);
-Route::patch('/products/{id}',   [ProductController::class, 'update']);
-
-
+Route::patch('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
