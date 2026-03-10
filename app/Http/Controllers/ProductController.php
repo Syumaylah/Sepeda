e359240a20d3f90ec20526efb48126dc4697d6a7
MunaFatinah <munafatinaha@gmail.com>
2026-03-10 15:30:32 +0700

    menambahkan index di product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            [
+                "id"       => 1,
+                "nama"     => "Sepeda MTB Polygon Siskiu T5",
+                "kategori" => "MTB",
+                "harga"    => 4500000,
+                "stok"     => 8,
+            ],
+            [
+                "id"       => 2,
+                "nama"     => "Sepeda Lipat Dahon Speed D8",
+                "kategori" => "Lipat",
+                "harga"    => 6200000,
+                "stok"     => 5,
+            ],
+            [
+                "id"       => 3,
+                "nama"     => "Sepeda Road Polygon Helios C3",
+                "kategori" => "Road",
+                "harga"    => 8900000,
+                "stok"     => 3,
+            ],
+        ];
+
+        return response()->json($products);
+    }
 }
